ed2658ee0266dc45f664d0bddc531ca90e5345c0
DrPoseidon <57911068+DrPoseidon@users.noreply.github.com>
2020-05-12 20:28:53 +0300

    Add files via upload

---
+++ src/DataFixtures/UsersFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UsersFixture extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setName('Anton Popov');
+        $user->setEmail('a.popov.d@gmail.com');
+        $user->setRoles(array('ROLE_ADMIN'));
+        $password = $this->encoder->encodePassword($user, 'pass_1234');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
